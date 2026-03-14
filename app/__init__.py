aa960e762e925902e3c1c97c17352a341be2648e
Yashi Saini <yashisaini718@gmail.com>
2026-03-14 20:56:54 +0530

    done this

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ login_manager.login_view='users.login'
 login_manager.login_message_category='info'
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(__name__)  #flask application object
     app.config.from_object(Config) 
 
     db.init_app(app)

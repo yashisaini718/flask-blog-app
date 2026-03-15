60d126620549d727475d9b42f5f4a2cffa00ce58
Yashi Saini <yashisaini718@gmail.com>
2026-03-15 20:14:16 +0530

    db statement deletion from main file

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
-from app import create_app, db
+from app import create_app
 
 app=create_app()
 
-with app.app_context():
-    db.create_all()
 # this allows automatic updates to be displayed in the website (real-time updation)
 if __name__=="__main__":
     app.run(debug=True)

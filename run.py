b459fa7cf615b9c22453efb4e473d440b3f51e2d
Yashi Saini <yashisaini718@gmail.com>
2026-03-15 19:58:56 +0530

    added creation of dattabase as soon as app is created

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
 
 app=create_app()
+
+with app.app_context():
+    db.create_all()
 # this allows automatic updates to be displayed in the website (real-time updation)
 if __name__=="__main__":
     app.run(debug=True)
